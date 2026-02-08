bc9c5d4496b1c19517c35150195fb898aab74c20
pablo mansilla martinelli <pablo.0698@hotmail.com>
2026-02-08 16:21:27 -0500

    implemented girl customization

---
+++ Assets/_ProjectFolder/Code/Scripts/Database/Customization/Resolvers/SkeletonBindHandler.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D.Animation;
+
+namespace Unity.Customization
+{
+    public class SkeletonBindHandler : MonoBehaviour
+    {
+        [SerializeField] private SkeletonAsset _skeleton;
+        [SerializeField] private Transform _root;
+        [SerializeField] private int _pixelPerUnit = 1024;
+
+        [ContextMenu("Bind Skeleton")]
+        private void BindSkeleton()
+        {
+            Dictionary<string, Transform> transforms = new();
+            foreach (Transform transform in _root.GetComponentsInChildren<Transform>())
+            {
+                if (!transforms.ContainsKey(transform.name))
+                    transforms.Add(transform.name, transform);
+            }
+            
+            foreach (var boneData in _skeleton.GetSpriteBones())
+            {
+                if (!transforms.TryGetValue(boneData.name, out Transform target))
+                    continue;
+
+                target.localPosition = boneData.position / _pixelPerUnit;
+                target.localRotation = boneData.rotation;
+            }
+        }
+    }
+}
